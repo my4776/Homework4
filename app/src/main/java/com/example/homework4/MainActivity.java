5f7a3e18fd684d6b8118f6ffc4631ca363047e8c
my4776 <my4776@bs.amalnet.k12.il>
2025-01-30 22:43:29 +0200

    misssion 4, seven boz.

--- app/src/main/java/com/example/homework4/MainActivity.java
+++ app/src/main/java/com/example/homework4/MainActivity.java
@@ -24,10 +24,9 @@ public class MainActivity extends AppCompatActivity {
     public void Albert(View view) {
         growUp++;
         btn.setText("This is a click number: "+growUp);
-        if (growUp==6)
+        if (growUp%7==0)
         {
-            btn.setText("Enough to click. Go to new start!");
-            growUp = 0;
+            btn.setText("Boom!");
         }
     }
 }
